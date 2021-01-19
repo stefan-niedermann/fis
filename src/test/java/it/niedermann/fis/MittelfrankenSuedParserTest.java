5be2eeda2dcdf2bcd4bf961619076e2c4d657a5d
Stefan Niedermann <info@niedermann.it>
2021-01-19 22:41:48 +0100

    Move test samples to resources

--- src/test/java/it/niedermann/fis/MittelfrankenSuedParserTest.java
+++ src/test/java/it/niedermann/fis/MittelfrankenSuedParserTest.java
@@ -2,63 +2,25 @@ package it.niedermann.fis;
 
 import it.niedermann.fis.operation.OperationInformationDto;
 import it.niedermann.fis.operation.parser.OperationFaxParser;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MittelfrankenSuedParserTest {
 
-    final static String ocr = "12/12/2012 13:13 +4998 987654321 FF MUSTERWEHR 5, 81/81\n" +
-            "BF en ES Taten EEE ET\n" +
-            "Sraaunn FAX sunnun FAX annmn- FAX =r--7u FAX uumun FAX mrz7u0 FAX enmmnmm\n" +
-            ". Absender : ILS MITTELFRANKEN SÜD ; ;\n" +
-            "* Fax : +49 (9876) / 5432101\n" +
-            "' Termin z ' )\n" +
-            "Einsatznummer: B 5.3 201024 630 ; ; ' ;.\n" +
-            "rk anumum  MITTEILER -4=nnnuheunenrer rennen\n" +
-            "anna akkamn an amr Eu rumnm EINSATZORT -+u-=nuuur en nenn Eee emnnn em nne\n" +
-            "; Straße : Musterstraße ;\n" +
-            "Haus-Nr. :\n" +
-            "ort : 99999 Musterdorf - Mustergemeinde Mustergemeinde -\n" +
-            "Objekt Ce | |\n" +
-            "Station : 0 ; ;\n" +
-            "‚ X=1234567.89\n" +
-            "Y=9876543. 21 ; ; ; ; ;\n" +
-            "A\n" +
-            "* straße : Haus-Nr. : ;\n" +
-            "Ort a '\n" +
-            "Objekt\n" +
-            "Station\n" +
-            "ELLE au ku nun aa mn nEEEEEEn PATIENT ----mrneeenmmn nennen memnn nenne\n" +
-            "Bm EINSATZGRUND ==mnne men\n" +
-            "Schlagw.: #B1014#im Freien#Abfall-, Müll-, Papiercontainer _\n" +
-            "Stichwort: B 1 ' '\n" +
-            "A uumamum EINSATZMITTEL «nn nn m\n" +
-            "; Name : 9.8.7 RH FF Musterwehr ;\n" +
-            "; Alarmiert. ; 12.12.2012 14:10:15\n" +
-            "Aus : ; ; ; ;\n" +
-            "Name : Florian Musterwehr 24/1\n" +
-            "Alarmiert : 12.12.2012 14:10:15\n" +
-            "Aus : ;\n" +
-            "Name : Florian Roth Land 7/8 ; .\n" +
-            "; Alarmiert : 12.12.2012 14:10:15\n" +
-            "Aus : ; .\n" +
-            "; Name : Florian Roth Länd 9/5 ; ;\n" +
-            "Alarmiert : 12.12.2012 14:16:15\n" +
-            "Aus ;\n" +
-            "Name ;. Florian Mustergemeinde 14/5 ;\n" +
-            "Alarmiert : 12.12.2012 14:10:16\n" +
-            "Aus BL ; ;\n" +
-            "na nuanun BEMERKUNG -=---- ru reknnamnmn Eee een nue)\n" +
-            "Container qualmt leicht - vmtl. heiße Asche (sichtbar) .\n" +
-            "im Gelände ehem. Brennerei ,\n" +
-            "een ALARMFAX ENDE Akememaneneenmeneeeee nennen\n" +
-            "Kb .";
+    private String getSample(@SuppressWarnings("SameParameterValue") int number) throws IOException {
+        return IOUtils.toString(new ClassPathResource("samples/mittelfranken-sued/" + number + ".txt").getInputStream(), UTF_8);
+    }
 
     @Test
-    public void parseTest() {
+    public void parseTest() throws IOException {
         final OperationFaxParser parser = OperationFaxParser.create("mittelfranken-sued");
-        final OperationInformationDto dto = parser.parse(ocr);
+        final OperationInformationDto dto = parser.parse(getSample(1));
 
         assertEquals("", dto.number);
 
@@ -79,6 +41,6 @@ public class MittelfrankenSuedParserTest {
         assertEquals("Mustergemeinde 14/5", dto.vehicles[4]);
 
         assertEquals("Container qualmt leicht - vmtl. heiße Asche (sichtbar)\n" +
-                        "im Gelände ehem. Brennerei", dto.note);
+                "im Gelände ehem. Brennerei", dto.note);
     }
 }

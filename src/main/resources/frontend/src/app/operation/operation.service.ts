8157f80bcacd18122572ea7bcd5182f03ae287ed
Stefan Niedermann <info@niedermann.it>
2021-01-23 16:05:45 +0100

    Implement highlighting

--- src/main/resources/frontend/src/app/operation/operation.service.ts
+++ src/main/resources/frontend/src/app/operation/operation.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Observable, of} from "rxjs";
+import {Observable} from "rxjs";
 import {WebSocketService} from "../web-socket.service";
 import {map} from "rxjs/operators";
 import {Operation} from "../domain/operation";
@@ -14,27 +14,7 @@ export class OperationService {
   constructor(
     private webSocket: WebSocketService
   ) {
-    this.operation$ = of({
-      "keyword": "B 1",
-      "number": "",
-      "street": "Musterstraße",
-      "location": "99999 Musterdorf - Mustergemeinde",
-      "obj": "",
-      "tags": [
-        "B1014",
-        "im Freien",
-        "Abfall-, Müll-, Papiercontainer"
-      ],
-      "vehicles": [
-        "9.8.7 RH FF Musterwehr",
-        "Musterwehr 24/1",
-        "Musterkreis Land 7/8",
-        "Musterkreis Land 9/5",
-        "Mustergemeinde 14/5"
-      ],
-      "note": "Container qualmt leicht - vmtl. heiße Asche (sichtbar)\nim Gelände ehem. Brennerei"
-    });
-      //this.webSocket.receiveCurrentOperation();
+    this.webSocket.receiveCurrentOperation();
   }
 
   public getActiveOperation(): Observable<Operation> {

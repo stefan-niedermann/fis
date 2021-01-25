bd22428816df973ae3c31145ee4f1ca2f4fcf7f6
Stefan Niedermann <info@niedermann.it>
2021-01-25 19:17:16 +0100

    Remove not working CLI

--- src/main/java/it/niedermann/fis/FisApplication.java
+++ src/main/java/it/niedermann/fis/FisApplication.java
@@ -4,48 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.io.Console;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
 @SpringBootApplication
 @EnableScheduling
 public class FisApplication {
 
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final Console console = System.console();
-
     public static void main(String[] args) {
-        final var mutableArgs = new ArrayList<>(Arrays.asList(args));
-
-        if (Arrays.stream(args).noneMatch(arg -> arg.startsWith("--ftp.user="))) {
-            System.out.print("FTP username: ");
-            mutableArgs.add("--ftp.user=" + scanner.next());
-        }
-
-        if (Arrays.stream(args).noneMatch(arg -> arg.startsWith("--ftp.password="))) {
-            System.out.print("FTP password: ");
-            mutableArgs.add("--ftp.user=" + nextSecret());
-        }
-
-        if (Arrays.stream(args).noneMatch(arg -> arg.startsWith("--weather.key="))) {
-            System.out.print("Weather API Key: ");
-            mutableArgs.add("--weather.key=" + nextSecret());
-        }
-
-        SpringApplication.run(FisApplication.class, mutableArgs.toArray(String[]::new));
-    }
-
-    /**
-     * Tries to hide the entered value from the {@link Console}.
-     * In case the {@link System#console()} doesn't support this, we will fall back to a visible value.
-     */
-    private static String nextSecret() {
-        if (console == null) {
-            return scanner.next();
-        } else {
-            return Arrays.toString(console.readPassword());
-        }
+        SpringApplication.run(FisApplication.class, args);
     }
 }

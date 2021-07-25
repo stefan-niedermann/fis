b387634df64c82e3c0a6d9bfd53b9176e47abd08
Stefan Niedermann <info@niedermann.it>
2021-07-25 16:55:00 +0200

    Remove @angular/router

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,5 +1,4 @@
 import {TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 
@@ -7,7 +6,6 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
         HttpClientTestingModule,
       ],
       declarations: [

bca977850b0f5bc0bf228c88268d76ad8d5d4021
Stefan Niedermann <info@niedermann.it>
2021-07-25 20:09:07 +0200

    Add unit tests for OperationLocation

--- frontend/src/app/operation/operation-location/operation-location.pipe.spec.ts
+++ frontend/src/app/operation/operation-location/operation-location.pipe.spec.ts
@@ -1,8 +1,33 @@
 import {OperationLocationPipe} from './operation-location.pipe';
+import {Operation} from "../../domain/operation";
 
 describe('OperationLocationPipe', () => {
+  const pipe = new OperationLocationPipe();
+
   it('create an instance', () => {
-    const pipe = new OperationLocationPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return an empty string for falsy values', () => {
+    expect(pipe.transform(false as any)).toEqual('');
+    expect(pipe.transform(null)).toEqual('');
+    expect(pipe.transform(undefined)).toEqual('');
+    expect(pipe.transform(0 as any)).toEqual('');
+    expect(pipe.transform({} as any)).toEqual('');
+    expect(pipe.transform(NaN as any)).toEqual('');
+  })
+
+  it('should transform location, street, number and object of an operation into one string', () => {
+    const complete = (operation: Partial<Operation>) => pipe.transform(Object.assign({
+      street: '',
+      number: '',
+      obj: '',
+      location: ''
+    }, operation) as Operation);
+
+    expect(complete({street: 'Samplestreet'})).toEqual('Samplestreet');
+    expect(complete({street: 'Samplestreet', number: '15'})).toEqual('Samplestreet 15');
+    expect(complete({location: 'Foo'})).toEqual('Foo');
+    expect(complete({obj: 'Bar'})).toEqual('Bar');
+  })
 });

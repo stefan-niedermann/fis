5df7566ac28d14a079a7f8fa57f39f5993a3f71d
Stefan Niedermann <info@niedermann.it>
2021-08-11 15:59:58 +0200

    Refactor e2e tests

--- frontend/cypress/integration/spec.ts
+++ frontend/cypress/integration/spec.ts
@@ -1,56 +1,75 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.intercept({ method: 'GET', url: '/operation' }, {})
-    cy.intercept({ method: 'GET', url: '/weather', }, {
-      temperature: 19.5,
-      icon: 'cloudy',
-      isDay: true
-    }
-    )
-    cy.intercept({ method: 'GET', url: '/socket/info*', }, {})
+describe('JarFIS main screen', () => {
 
-    cy.visit('/')
-    cy.contains('Uhr')
+  beforeEach(() => {
+    cy.intercept('/operation', '')
+    cy.intercept('/weather', sampleWeather)
+    cy.intercept('/parameter', sampleParameter)
+    cy.intercept('/socket/*', {})
+  });
+
+
+  it('Visits the initial project page', () => {
+    cy.visit('/');
+    verifyInfo();
   })
 
   it('Should fetch and display running operations on startup', () => {
-    cy.intercept({ method: 'GET', url: '/operation', }, {
-      "keyword": "B 1",
-      "number": "",
-      "street": "Musterstraße",
-      "location": "99999 Musterdorf - Mustergemeinde",
-      "obj": "",
-      "tags": [
-        "B1014",
-        "im Freien",
-        "Abfall-, Müll-, Papiercontainer"
-      ],
-      "vehicles": [
-        "9.8.7 RH FF Musterwehr",
-        "Musterwehr 24/1",
-        "Musterkreis Land 7/8",
-        "Musterkreis Land 9/5",
-        "Mustergemeinde 14/5"
-      ],
-      "note": "Container qualmt leicht - vmtl. heiße Asche (sichtbar)\nim Gelände ehem. Brennerei"
-    }
-    )
-    cy.intercept({ method: 'GET', url: '/weather', }, {})
-    cy.intercept({ method: 'GET', url: '/parameter' }, {
-      language: 'de',
-      operation: {
-        duration: 5,
-        highlight: 'land'
-      }
-    }
-    )
-    cy.intercept({ method: 'GET', url: '/socket/info*', }, {})
-    cy.intercept({ method: 'POST', url: '/socket/*', }, {})
-    cy.visit('/')
-    cy.contains('B1')
-    cy.contains('Musterstraße')
-    cy.contains('Musterdorf')
-    cy.contains('Musterwehr')
+    cy.intercept('/operation', sampleOperation)
+    cy.visit('/');
+    verifyOperation();
   })
+
+  // FIXME how to mock the websocket connection?
+  xit('Gets an operation pushed', () => {
+    cy.visit('/');
+    verifyInfo();
+    // mockSocket.send(JSON.stringify(sampleOperation));
+    verifyOperation();
+    cy.wait(500)
+    verifyInfo();
+  })
+
+  const sampleWeather = {
+    temperature: 19.5,
+    icon: 'cloudy',
+    isDay: true
+  }
+
+  const sampleParameter = {
+    language: 'de',
+    operation: {
+      duration: 5,
+      highlight: 'land'
+    }
+  }
+
+  const sampleOperation = {
+    "keyword": "B 1",
+    "number": "",
+    "street": "Musterstraße",
+    "location": "99999 Musterdorf - Mustergemeinde",
+    "obj": "",
+    "tags": [
+      "B1014",
+      "im Freien",
+      "Abfall-, Müll-, Papiercontainer"
+    ],
+    "vehicles": [
+      "9.8.7 RH FF Musterwehr",
+      "Musterwehr 24/1",
+      "Musterkreis Land 7/8",
+      "Musterkreis Land 9/5",
+      "Mustergemeinde 14/5"
+    ],
+    "note": "Container qualmt leicht - vmtl. heiße Asche (sichtbar)\nim Gelände ehem. Brennerei"
+  }
+
+  const verifyInfo = () => {
+    cy.contains('Uhr')
+  }
+
+  const verifyOperation = () => {
+    cy.contains(sampleOperation.street)
+  }
 
 })

04ee10b41fbaddb206507e6afa0674fcab1af5ec
Stefan Niedermann <info@niedermann.it>
2021-08-11 14:36:26 +0200

    Add additional e2e test for active operatiosn on startup

--- frontend/cypress/integration/spec.ts
+++ frontend/cypress/integration/spec.ts
@@ -1,27 +1,56 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/operation',
-      },
-      {}
+    cy.intercept({ method: 'GET', url: '/operation' }, {})
+    cy.intercept({ method: 'GET', url: '/weather', }, {
+      temperature: 19.5,
+      icon: 'cloudy',
+      isDay: true
+    }
     )
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/weather',
-      },
-      {}
-    )
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/socket/info*',
-      },
-      {}
-    )
+    cy.intercept({ method: 'GET', url: '/socket/info*', }, {})
+
     cy.visit('/')
     cy.contains('Uhr')
   })
+
+  it('Should fetch and display running operations on startup', () => {
+    cy.intercept({ method: 'GET', url: '/operation', }, {
+      "keyword": "B 1",
+      "number": "",
+      "street": "Musterstraße",
+      "location": "99999 Musterdorf - Mustergemeinde",
+      "obj": "",
+      "tags": [
+        "B1014",
+        "im Freien",
+        "Abfall-, Müll-, Papiercontainer"
+      ],
+      "vehicles": [
+        "9.8.7 RH FF Musterwehr",
+        "Musterwehr 24/1",
+        "Musterkreis Land 7/8",
+        "Musterkreis Land 9/5",
+        "Mustergemeinde 14/5"
+      ],
+      "note": "Container qualmt leicht - vmtl. heiße Asche (sichtbar)\nim Gelände ehem. Brennerei"
+    }
+    )
+    cy.intercept({ method: 'GET', url: '/weather', }, {})
+    cy.intercept({ method: 'GET', url: '/parameter' }, {
+      language: 'de',
+      operation: {
+        duration: 5,
+        highlight: 'land'
+      }
+    }
+    )
+    cy.intercept({ method: 'GET', url: '/socket/info*', }, {})
+    cy.intercept({ method: 'POST', url: '/socket/*', }, {})
+    cy.visit('/')
+    cy.contains('B1')
+    cy.contains('Musterstraße')
+    cy.contains('Musterdorf')
+    cy.contains('Musterwehr')
+  })
+
 })

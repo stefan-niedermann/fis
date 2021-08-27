a3a0ca109ef45fd189fb892de8b32d24f669a80d
Stefan Niedermann <info@niedermann.it>
2021-08-27 10:53:55 +0200

    Migrate form TSLint to ESLint and use in pipeline

--- frontend/src/app/operation/operation-location/operation-location.component.ts
+++ frontend/src/app/operation/operation-location/operation-location.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Operation} from "../../domain/operation";
 
 @Component({
@@ -6,14 +6,9 @@ import {Operation} from "../../domain/operation";
   templateUrl: './operation-location.component.html',
   styleUrls: ['./operation-location.component.scss']
 })
-export class OperationLocationComponent implements OnInit {
+export class OperationLocationComponent {
 
   @Input()
   operation: Operation;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  
 }

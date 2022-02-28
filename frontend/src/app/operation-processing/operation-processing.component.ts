588e517cef18f31ab92edaac6913bc5c5adf36d9
Stefan Niedermann <info@niedermann.it>
2022-02-28 14:52:42 +0100

    Fix lint (empty lifecycle method)

--- frontend/src/app/operation-processing/operation-processing.component.ts
+++ frontend/src/app/operation-processing/operation-processing.component.ts
@@ -1,15 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-operation-processing',
   templateUrl: './operation-processing.component.html',
   styleUrls: ['./operation-processing.component.scss']
 })
-export class OperationProcessingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class OperationProcessingComponent {
 
 }

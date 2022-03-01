6e674f29acf1f3f2c5ac2eadcb0dbef19eb1a380
Stefan Niedermann <info@niedermann.it>
2022-03-01 12:07:06 +0100

    Fix missing import

--- src/main/java/it/niedermann/fis/weather/provider/DayNightIcon.java
+++ src/main/java/it/niedermann/fis/weather/provider/DayNightIcon.java
@@ -1,5 +1,7 @@
 package it.niedermann.fis.weather.provider;
 
+import it.niedermann.fis.main.model.WeatherIconDto;
+
 public record DayNightIcon(
         WeatherIconDto day,
         WeatherIconDto night

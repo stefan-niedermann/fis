fb54c6adbea723261df47e448310f4fe27b058c5
Stefan Niedermann <info@niedermann.it>
2022-03-02 15:15:43 +0100

    Use mrm for clearing ftp

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -13,20 +13,10 @@ declare namespace Cypress {
 }
 
 Cypress.Commands.add('clearFtpServer', () => {
-  // ftp('ls -all').then((result) => {
-  //   console.log(result.code)
-  //   console.log(result.stdout)
-  //   console.log(result.stderr)
-  // })
-  // ftp(`rm -r ${Cypress.env('FTP_DIR')}/*`)
+  ftp(`mrm -r ${Cypress.env('FTP_DIR')}/*`)
 })
 
 Cypress.Commands.add('sendFaxToFtpServer', (type) => {
-  // cy.exec('ls -all').then((result) => {
-  //   console.log(result.code)
-  //   console.log(result.stdout)
-  //   console.log(result.stderr)
-  // })
   ftp(`put -O ${Cypress.env('FTP_DIR')} cypress/assets/${type}.pdf`)
 })
 

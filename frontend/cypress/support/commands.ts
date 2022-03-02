9f6b4f1b9f55a2e470620fa85afd334aaea1679a
Stefan Niedermann <info@niedermann.it>
2022-03-02 13:10:34 +0100

    Use environment variables

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -1,11 +1,11 @@
-const ftp_user = 'testuser'
-const ftp_password = 'testuser'
-const ftp_host = 'localhost'
+const ftp_host = process.env.CYPRESS_FTP_HOST
+const ftp_user = process.env.CYPRESS_FTP_USER
+const ftp_pass = process.env.CYPRESS_FTP_PASS
 
 Cypress.Commands.add('clearFtpServer', () => {
-  cy.exec(`lftp -u ${ftp_user},${ftp_password} -e "set ssl:verify-certificate no; rm -r ./; quit;" ${ftp_host}`)
+  cy.exec(`lftp -u ${ftp_user},${ftp_pass} -e "set ssl:verify-certificate no; rm -r ./; quit;" ${ftp_host}`)
 })
 
 Cypress.Commands.add('sendFaxToFtpServer', (type) => {
-  cy.exec(`lftp -u ${ftp_user},${ftp_password} -e "set ssl:verify-certificate no; mirror --reverse ../assets/${type}.pdf ./ --verbose; quit;" ${ftp_host}`)
+  cy.exec(`lftp -u ${ftp_user},${ftp_pass} -e "set ssl:verify-certificate no; mirror --reverse ../assets/${type}.pdf ./ --verbose; quit;" ${ftp_host}`)
 })

d4b6d5849f470c50db64d869178e880652d9047a
Niedermann IT-Dienstleistungen <stefan-niedermann@users.noreply.github.com>
2022-03-11 14:33:41 +0100

    Publish docker image

--- src/main/java/it/niedermann/fis/operation/remote/OperationFTPClientFactory.java
+++ src/main/java/it/niedermann/fis/operation/remote/OperationFTPClientFactory.java
@@ -19,7 +19,7 @@ class OperationFTPClientFactory {
         if (!ftpClient.login(config.ftp().username(), config.ftp().password())) {
             throw new IllegalArgumentException("🚒 Could not connect to FTP server + " + config.ftp().host() + ". Please check FTP credentials.");
         }
-        logger.info("🚒 Connected to FTP server " + config.ftp().host() + ", polling each " + config.ftp().pollInterval() / 1_000 + " seconds.");
+        logger.info("🚒 Connected to FTP server " + ftpClient.getRemoteAddress() + " on port " + ftpClient.getRemotePort() + ", polling each " + config.ftp().pollInterval() / 1_000 + " seconds.");
         return ftpClient;
     }
 }
